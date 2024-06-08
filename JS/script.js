addc608724329d30ea98801bbe21ec03440ada52
obed renteria <ing7obed@gmail.com>
2024-06-08 15:56:38 -0600

    fixBtnsize

--- JS/script.js
+++ JS/script.js
@@ -9,6 +9,13 @@ window.onscroll = ()=>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
   }
+  document.querySelector('#learn-more').onclick = () =>{ 
+    document.querySelector('.popup-image').style.display = 'block';
+    }
+
+document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'none';
+}
 
 /*  scroll reveal */
 ScrollReveal({
