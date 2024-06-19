365637de59595d362a6c049490f0da075a322f21
obed renteria <ing7obed@gmail.com>
2024-06-19 09:08:22 -0600

    centerpopupbox

--- JS/script.js
+++ JS/script.js
@@ -11,20 +11,22 @@ window.onscroll = ()=>{
   }
   document.querySelector('#btnwebsite').onclick = () =>{ 
     document.querySelector('.popup-image').style.display = 'block';
+    document.querySelector('#website-box').style.display = 'block';
   }
   document.querySelector('#btnwebapp').onclick = () =>{ 
     document.querySelector('.popup-image').style.display = 'block';
-    document.querySelector('.webapp-box').style.display = 'block';
+    document.querySelector('#webapp-box').style.display = 'block';
     }
   document.querySelector('#btnmore').onclick = () =>{ 
     document.querySelector('.popup-image').style.display = 'block';
-    document.querySelector('.more-box').style.display = 'block';
+    document.querySelector('#more-box').style.display = 'block';
   }
 
 document.querySelector('.popup-image span').onclick = () =>{
     document.querySelector('.popup-image').style.display = 'none';
-    document.querySelector('.webapp-box').style.display = 'none';
-    document.querySelector('.more-box').style.display = 'none';
+    document.querySelector('#website-box').style.display = 'none';
+    document.querySelector('#webapp-box').style.display = 'none';/*websitebox is not display none because it gets overlap by the other */
+    document.querySelector('#more-box').style.display = 'none';
 }
 
 /*  scroll reveal */
